854e1482851ec3d6a39efc03fb0a763b34e58279
ArtemPrihnich <artem.prihnich@gmail.com>
2022-03-22 15:55:16 +0200

    сделаны отзывчивые изображения

--- js/modal.js
+++ js/modal.js
@@ -28,20 +28,20 @@
   }
 })();
 
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open2]"),
-    closeMenuBtn: document.querySelector("[data-menu-close2]"),
-    menu: document.querySelector("[data-menu2]"),
-  };
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector("[data-menu-open2]"),
+//     closeMenuBtn: document.querySelector("[data-menu-close2]"),
+//     menu: document.querySelector("[data-menu2]"),
+//   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+//   refs.openMenuBtn.addEventListener("click", toggleMenu);
+//   refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-  }
-})();
+//   function toggleMenu() {
+//     refs.menu.classList.toggle("is-hidden");
+//   }
+// })();
 
 // (() => {
 //   const menuBtnRef = document.querySelector("data-menu-button");
